55200fce6656ca2cef1b475a292fc60cb4d06c5d
Sam Smets <sam@skipr.co>
2023-04-18 09:05:49 +0200

    Added dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:weather/src/core/dependency_injection.dart';
 import 'package:weather/src/weather_app.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
+
+  setupDependencies();
+
   runApp(const WeatherApp());
 }
--- lib/src/data/repositories/weather_repository/weather_repository_impl.dart
+++ lib/src/data/repositories/weather_repository/weather_repository_impl.dart
@@ -1,15 +1,17 @@
 import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:weather/src/data/repositories/weather_repository/weather_repository.dart';
 
-class WeatherRepositoryImpl implements WeatherRepository {
-  static const String _apiKey = '3d04382b74e74274b0e55636231804';
-  static const String _baseUrl = 'http://api.weatherapi.com/v1';
+const String baseUrl = 'http://api.weatherapi.com/v1';
 
+class WeatherRepositoryImpl implements WeatherRepository {
   @override
   Future<Map<String, dynamic>> fetchCurrentWeather(String city) async {
+    final String? apiKey = dotenv.env['WEATHER_API_KEY'];
+
     final response = await http.get(
-      Uri.parse('$_baseUrl/current.json?key=$_apiKey&q=$city&aqi=no'),
+      Uri.parse('$baseUrl/current.json?key=$apiKey&q=$city&aqi=no'),
     );
 
     if (response.statusCode == 200) {
